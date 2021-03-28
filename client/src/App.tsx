c68d671d12381dedf86a9cf7fe1f2c60c0300a76
Artemy-And <andruschak.artemy@gmail.com>
2021-03-28 11:54:09 +0300

    layout app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,37 @@ import "destyle.css";
 import React from "react";
 
 const App: React.FC = () => {
-  return <div>list of RATS That should stay away from my DAMN BINS</div>;
+  let array = ['']
+  return (
+      <div className='container'>
+        <div className='mainInfo'>
+          <b><label htmlFor="rats">Select rat: </label></b>
+          <select
+              className='select'
+              name="rats"
+              id='rats'
+          >
+            {array.map((e: any, index: any) => {
+              return (
+                  <option
+                      value={e}
+                      key={index}
+                  > {e}</option>
+              )
+            })}
+          </select>
+        </div>
+        <div className='additionalInfo'>
+
+          <div className='info'>
+            <p><b>Width</b></p>
+            <p><b>Height</b></p>
+            <p><b>Nickname</b></p>
+          </div>
+
+        </div>
+      </div>
+  )
 };
 
 export default App;
